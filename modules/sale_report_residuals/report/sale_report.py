3669e96d55ed90da573ca35dd224b82423c5ca55
Alfredo Salata <alfredo.salata@rdsmoulding.com>
2018-11-23 11:31:48 +0100

    HF

--- modules/sale_report_residuals/report/sale_report.py
+++ modules/sale_report_residuals/report/sale_report.py
@@ -8,8 +8,9 @@ class SaleReport(models.Model):
     qty_to_deliver = fields.Float('Qty To Deliver', readonly=True)
     untaxed_amount_to_deliver = fields.Float('Untaxed Amount To Deliver', readonly=True)
     commitment_date = fields.Datetime('Commitment Date', readonly=True)
-
+    
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
+        fields['commitment_date'] = ", s.commitment_date as commitment_date"
         fields['qty_to_deliver'] = ", sum(greatest(l.product_uom_qty - l.qty_delivered, 0) / u.factor * u2.factor) as qty_to_deliver"
         fields['untaxed_amount_to_deliver'] = ", sum( l.price_unit * (greatest(l.product_uom_qty - l.qty_delivered, 0) / u.factor * u2.factor) ) as untaxed_amount_to_deliver"
         groupby += ', s.commitment_date'
