d395cce02f3ae785b015c5595ab1712a400b1398
Alfredo Salata <alfredo.salata@rdsmoulding.com>
2018-11-30 15:18:03 +0100

    hf

--- modules/rds_production/models/product_product.py
+++ modules/rds_production/models/product_product.py
@@ -5,12 +5,3 @@ class ProductProduct(models.Model):
 
     sale_ok = fields.Boolean('Can be Sold', default=True)
     purchase_ok = fields.Boolean('Can be Purchased', default=True)
-    
-    property_account_income_id = fields.Many2one('account.account', company_dependent=True,
-        string="Income Account",
-        domain=[('deprecated', '=', False)],
-        help="Keep this field empty to use the default value from the product category.")
-    property_account_expense_id = fields.Many2one('account.account', company_dependent=True,
-        string="Expense Account",
-        domain=[('deprecated', '=', False)],
-        help="Keep this field empty to use the default value from the product category. If anglo-saxon accounting with automated valuation method is configured, the expense account on the product category will be used.")
