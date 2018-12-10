867b93e7033edb31da63c734fc5422cbdfe194f3
Alfredo Salata <alfredo.salata@rdsmoulding.com>
2018-12-10 18:07:17 +0100

    Aggiunto local. termini di pagamento e distinta CBI

--- modules/rds_customizations_account/models/account_payment_term.py
+++ modules/rds_customizations_account/models/account_payment_term.py
@@ -32,8 +32,7 @@ class AccountPaymentTerm(models.Model):
                     next_first_date = next_date + relativedelta(day=1, months=1)  # Getting 1st of next month
                     next_date = next_first_date + relativedelta(days=line.days - 1)
                 elif line.option == 'end_of_month_after_end_of_month':
-                    next_first_date = next_date + relativedelta(day=31, months=1)  # Getting 1st of next month
-                    next_date = next_first_date + relativedelta(days=line.day_of_the_month, months=line.days//30, day=31)
+                    next_date = next_date + relativedelta(days=line.day_of_the_month, months=line.days//30, day=31)
                 elif line.option == 'day_following_month':
                     next_date += relativedelta(day=line.days, months=1)
                 elif line.option == 'day_current_month':
